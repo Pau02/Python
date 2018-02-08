e58af3661c2b4e8888b017b6b3cbc2ff6b2da7f5
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-02-08 12:08:24 +0100

    Create ejercicio-condicion-chunga.py

---
+++ ejercicio-condicion-chunga.py
@@ -0,0 +1,8 @@
+# coding: utf8
+# Pau Martin Fernandez
+
+numero = input ("Escribe un Numero")
+
+if numero %2 == 0 or ((numero<=-10) and (numero<=40)) or(numero==-numero):
+	
+    print "Numero Correcto"
