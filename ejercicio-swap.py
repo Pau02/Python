aa0bf7d39a56fc10492252ae8034d909b1784278
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-02-08 12:12:32 +0100

    Create ejercicio-swap.py

---
+++ ejercicio-swap.py
@@ -0,0 +1,13 @@
+# coding: utf8
+# Pau Martin Fernandez
+
+mano_der="movil"
+mano_izq="bocadillo"
+
+mano_temp=mano_der
+mano_der=mano_izq
+mano_izq=mano_temp
+
+print mano_der
+print mano_izq
+print mano_temp
