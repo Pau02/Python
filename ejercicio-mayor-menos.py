cd36724707ea64baf7457e1460d30029913f5fc5
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-02-09 12:58:00 +0100

    Create ejercicio-mayor-menos.py

---
+++ ejercicio-mayor-menos.py
@@ -0,0 +1,18 @@
+# coding: utf8
+# Pau Martin Fernandez
+
+Valor1 = input ("Introducir Numero")
+Valor2 = input ("Introducir Numero")
+Valor3 = input ("Introducir Numero")
+
+if (Valor1 > Valor2) and (Valor1 > Valor3):
+    print "Valor1 es mayor"
+    if (Valor2 < Valor3):
+	    print "Valor2 es menor"
+    else:
+		"Valor3 es menor"		  
+else:
+    if (Valor2 > Valor1) and (Valor2 > Valor3):
+	    print "Valor2 es Mayor"
+	else
+		print "Valor3 es Mayor"
