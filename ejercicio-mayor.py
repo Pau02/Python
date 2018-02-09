a1d44bdf75fa405ef4c18b86f8b0f80b8b9c597c
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-02-09 11:58:13 +0100

    Create ejercicio-mayor.py

---
+++ ejercicio-mayor.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# Pau Martin Fernandez
+
+Valor1 =  input ("Introducir Numero")
+Valor2 = input ("Introducir Numero")
+
+if Valor1 > Valor2:
+    print "Valor 1 es mayor"
+else:
+    if Valor2 == Valor1:
+		print "Son iguales."
+	
+    else:
+	    print "Valor 2 es Mayor."
