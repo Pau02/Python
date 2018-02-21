9fb14efb15d2d8e3940b38123b887a1151e6f265
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-02-21 23:05:43 +0100

    Create Ejercicio-Mayor.py

---
+++ Ejercicio-Mayor.py
@@ -0,0 +1,14 @@
+# coding : utf8
+
+Uno = input ("Escriba un Número)
+Dos = input ("Escriba un Número)
+
+if A > B :
+    print A, "Es mas Grande Que", B
+    
+    
+elif B > A :
+    print B, "Es mas Grande Que", A
+
+else : 
+    print "Son iguales"
