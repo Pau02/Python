7b0f9634d01dc92d783cdc21a8bc2e94eb174872
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-03-06 00:09:40 +0100

    Create bucle.py

---
+++ bucle.py
@@ -0,0 +1,13 @@
+#coding: utf8
+#Pau Martín Fernández
+
+salir="N"
+anyo=1999
+
+while(salir=="N"):
+    print "Informes de este año",anyo
+	
+    anyo = anyo +1
+    
+    if (anyo > 2003) :
+        salir = "S"
