ab0f8cca71b039d02bacdda0471e0f7794bf0fa9
Pau02 <36186966+Pau02@users.noreply.github.com>
2018-03-16 12:59:30 +0100

    Create mejora-ejercicio-giro-derecha.py

---
+++ mejora-ejercicio-giro-derecha.py
@@ -0,0 +1,27 @@
+#coding: utf8 
+#Pau Martín Fernandez
+import os
+numero=1 
+maximo=8
+vueltas=input ("Cuantas vueltas quieres?: ") 
+giro=raw_input ("Hacia donde giro?: ")
+salir="N" 
+while(salir=="N"): 
+    if(numero%8==1) or (numero%8==2): 
+        print ( numero,"Arriba") 
+    if(numero%8==3) or (numero%8==4): 
+        if(giro.upper=="D"):
+            print ( numero,"Izquierda")
+        else:
+			print (numero,"Izquierda")     
+    if(numero%8==5) or (numero%8==6): 
+        print ( numero,"Abajo") 
+    if(numero%8==7) or (numero%8==0):
+		if(giro.upper=="D"):
+			print (numero, "Izquierda")
+		else:
+			 print ( numero,"Derecha") 	
+    
+    numero = numero + 1
+    if(numero > vueltas * maximo): 
+		salir = "S"
